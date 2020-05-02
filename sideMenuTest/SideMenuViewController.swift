21af7ed4162f038d9ce6d21f244c03a4415e9959
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-03 00:46:06 +0900

    開始メソッド作成。

---
+++ sideMenuTest/SideMenuViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SideMenuViewController.swift
+//  sideMenuTest
+//
+//  Created by 福山帆士 on 2020/05/02.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .yellow
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ sideMenuTest/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  sideMenuTest
+//
+//  Created by 福山帆士 on 2020/05/02.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let contentVC = ContentViewController()
+    let sideMenuVC = SideMenuViewController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+        
+        self.addChild(contentVC)
+        self.view.addSubview(contentVC.view)
+        contentVC.didMove(toParent: self)
+        
+        self.addChild(sideMenuVC)
+        self.view.addSubview(sideMenuVC.view)
+        sideMenuVC.didMove(toParent: self)
+        
+        sideMenuVC.view.isHidden = true
+        self.view.bringSubviewToFront(sideMenuVC.view)
+        
+        presentsideMenu()
+    }
+    
+    func presentsideMenu() {
+        // viewWillApperを呼び出す
+        sideMenuVC.beginAppearanceTransition(true, animated: true)
+        sideMenuVC.view.isHidden = false
+        sideMenuVC.view.frame = sideMenuVC.view.frame.offsetBy(dx: -sideMenuVC.view.frame.size.width, dy: 0)
+        UIView.animate(withDuration: 0.3, delay: 2, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.8, options: .curveEaseIn, animations: {
+            let bounds = self.sideMenuVC.view.bounds
+            self.sideMenuVC.view.frame = CGRect(x: -bounds.size.width / 2, y: 0, width: bounds.size.width, height: bounds.size.height)
+        }, completion: { _ in
+            self.sideMenuVC.endAppearanceTransition()
+        })
+    }
+
+
+}
+
