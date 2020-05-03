1344bad1528381da044341b9fe389350a8d3cc3d
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-05-03 13:48:05 +0900

    親関係をview→content→sideにした。contentをnavigationに変更した。

--- sideMenuTest/ContentViewController.swift
+++ sideMenuTest/ContentViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ContentViewController: UIViewController {
+    
+    let sideMenuVC = SideMenuViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +18,57 @@ class ContentViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .blue
+        
+        self.addChild(sideMenuVC)
+        self.view.addSubview(sideMenuVC.view)
+        sideMenuVC.didMove(toParent: self)
+        
+        sideMenuVC.view.isHidden = true
+        self.view.bringSubviewToFront(sideMenuVC.view)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(menuButtonAction(_:)))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(dismissButtonAction(_:)))
     }
+    
+    @objc func menuButtonAction(_ button: UIBarButtonItem) {
+        presentsideMenu()
+        print("tap")
+    }
+    
+    @objc func dismissButtonAction(_ button: UIBarButtonItem) {
+        dismissMenu()
+    }
+    
+    // サイドメニュー開始
+    func presentsideMenu() {
+        // viewWillApperを呼び出す
+        print("present")
+        sideMenuVC.beginAppearanceTransition(true, animated: true)
+        sideMenuVC.view.isHidden = false
+        sideMenuVC.view.frame = sideMenuVC.view.frame.offsetBy(dx: -sideMenuVC.view.frame.size.width, dy: 0)
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.8, options: .curveEaseIn, animations: {
+            let bounds = self.sideMenuVC.view.bounds
+            self.sideMenuVC.view.frame = CGRect(x: -bounds.size.width / 2, y: 0, width: bounds.size.width, height: bounds.size.height)
+        }, completion: { _ in
+            // viewDidApperを呼び出す
+            self.sideMenuVC.endAppearanceTransition()
+            print("completion")
+        })
+    }
+    
+    // サイドメニュー終了
+    func dismissMenu() {
+        print("dismiss")
+        sideMenuVC.beginAppearanceTransition(false, animated: true)
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
+            self.sideMenuVC.view.frame = self.sideMenuVC.view.frame.offsetBy(dx: -self.sideMenuVC.view.frame.size.width / 2, dy: 0)
+        }, completion: {_ in
+            self.sideMenuVC.view.isHidden = true
+            self.sideMenuVC.endAppearanceTransition()
+        })
+    }
+    
+    
     
 
     /*
--- sideMenuTest/ViewController.swift
+++ sideMenuTest/ViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var menuButton: UIBarButtonItem!
-    
-    let contentVC = ContentViewController()
     let sideMenuVC = SideMenuViewController()
 
     override func viewDidLoad() {
@@ -21,55 +18,12 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = .red
         
-        self.addChild(contentVC)
-        self.view.addSubview(contentVC.view)
-        contentVC.didMove(toParent: self)
+        let contentVC = self.storyboard?.instantiateViewController(identifier: "navi")
+        self.addChild(contentVC!)
+        self.view.addSubview(contentVC!.view)
+        contentVC!.didMove(toParent: self)
         
-        self.addChild(sideMenuVC)
-        self.view.addSubview(sideMenuVC.view)
-        sideMenuVC.didMove(toParent: self)
-        
-        sideMenuVC.view.isHidden = true
-        self.view.bringSubviewToFront(sideMenuVC.view)
-        
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(dismissButtonAction(_:)))
     }
     
-    // サイドメニュー開始
-    func presentsideMenu() {
-        // viewWillApperを呼び出す
-        sideMenuVC.beginAppearanceTransition(true, animated: true)
-        sideMenuVC.view.isHidden = false
-        sideMenuVC.view.frame = sideMenuVC.view.frame.offsetBy(dx: -sideMenuVC.view.frame.size.width, dy: 0)
-        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.8, options: .curveEaseIn, animations: {
-            let bounds = self.sideMenuVC.view.bounds
-            self.sideMenuVC.view.frame = CGRect(x: -bounds.size.width / 2, y: 0, width: bounds.size.width, height: bounds.size.height)
-        }, completion: { _ in
-            // viewDidApperを呼び出す
-            self.sideMenuVC.endAppearanceTransition()
-        })
-    }
-    
-    // サイドメニュー終了
-    func dismissMene() {
-        sideMenuVC.beginAppearanceTransition(false, animated: true)
-        UIView.animate(withDuration: 0.3, delay: 2, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
-            self.sideMenuVC.view.frame = self.sideMenuVC.view.frame.offsetBy(dx: self.sideMenuVC.view.frame.size.width / 2, dy: 0)
-        }, completion: {_ in
-            self.sideMenuVC.view.isHidden = true
-            self.sideMenuVC.endAppearanceTransition()
-        })
-    }
-    
-    @IBAction func menuButtonAction(_ sender: Any) {
-        presentsideMenu()
-    }
-    
-    @objc func dismissButtonAction(_ button: UIBarButtonItem) {
-        dismissMene()
-    }
-    
-
-
 }
 
